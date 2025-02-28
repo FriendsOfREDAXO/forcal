795e6a01d7e5258ed27b843c44eb9007e0404b5a
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-28 15:07:32 +0100

    Update entries.php

--- pages/entries.php
+++ pages/entries.php
@@ -7,7 +7,7 @@
 
 use forCal\Handler\forCalHandler;
 use forCal\Manager\forCalFormManager;
-use forCal\Manager\forCalFormHelper;
+use forCal\Utils\forCalFormHelper;
 use forCal\Utils\forCalAttributesHelper;
 use forCal\Utils\forCalListHelper;
 use forCal\Utils\forCalUserPermission;
