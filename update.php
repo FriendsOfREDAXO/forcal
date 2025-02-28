288d8e05184aa53e8be4aa13901bd08885965e47
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-28 14:15:09 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,11 +1,22 @@
 <?php
+/**
+ * @author mail[at]doerr-softwaredevelopment[dot]com Joachim Doerr
+ * @package redaxo5
+ * @license MIT
+ */
 
-// Create user-category assignment table
+// Benutzer-Kategorie-Rechte-Tabelle erstellen
 rex_sql_table::get(rex::getTablePrefix() . 'forcal_user_categories')
     ->ensureColumn(new rex_sql_column('id', 'int(11) unsigned', false, null, 'auto_increment'))
-    ->ensureColumn(new rex_sql_column('user_id', 'int(11)', false))
-    ->ensureColumn(new rex_sql_column('category_id', 'int(11)', false))
+    ->ensureColumn(new rex_sql_column('user_id', 'int(11)'))
+    ->ensureColumn(new rex_sql_column('category_id', 'int(11)'))
     ->ensureColumn(new rex_sql_column('createdate', 'datetime', false, 'CURRENT_TIMESTAMP'))
-    ->ensureColumn(new rex_sql_column('updatedate', 'datetime', false, 'CURRENT_TIMESTAMP', 'on update CURRENT_TIMESTAMP'))
     ->setPrimaryKey('id')
     ->ensure();
+
+// Datapane-Struktur zum Backend hinzufügen
+if (rex_addon::get('forcal')->hasConfig()) {
+    $config = rex_addon::get('forcal')->getConfig();
+    $config['forcal_multiuser'] = 1;
+    rex_addon::get('forcal')->setConfig($config);
+}
