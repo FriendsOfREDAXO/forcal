31c76e4b7c55bdb53942028de4cb10f1c3eaa4e3
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-02 20:10:02 +0100

    Update update.php

--- update.php
+++ update.php
@@ -14,6 +14,19 @@ rex_sql_table::get(rex::getTablePrefix() . 'forcal_user_categories')
     ->setPrimaryKey('id')
     ->ensure();
 
+/**
+ * Erweitert den install.php Code, um die Tabelle für Medienberechtigungen zu erstellen
+ */
+
+// Neue Tabelle für Media-Berechtigungen erstellen
+rex_sql_table::get(rex::getTablePrefix() . 'forcal_user_media_permissions')
+    ->ensureColumn(new rex_sql_column('id', 'int(11) unsigned', false, null, 'auto_increment'))
+    ->ensureColumn(new rex_sql_column('user_id', 'int(11)'))
+    ->ensureColumn(new rex_sql_column('can_upload_media', 'tinyint(1)', false, '0'))
+    ->ensureColumn(new rex_sql_column('createdate', 'datetime', false, 'CURRENT_TIMESTAMP'))
+    ->setPrimaryKey('id')
+    ->ensure();
+
 // Datapane-Struktur zum Backend hinzufügen
 if (rex_addon::get('forcal')->hasConfig()) {
     $config = rex_addon::get('forcal')->getConfig();
